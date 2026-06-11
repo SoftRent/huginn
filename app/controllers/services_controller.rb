3dda041be8eeb3c54dcdb3195f1944f7899bd445
SoftRent <codewithneerb@gmail.com>
2026-06-11 20:49:14 +0530

    Initial commit

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,38 @@
+class ServicesController < ApplicationController
+  include SortableTable
+
+  before_action :upgrade_warning, only: :index
+
+  def index
+    set_table_sort sorts: %w[provider name global], default: { provider: :asc }
+
+    @services = current_user.services.reorder(table_sort).page(params[:page])
+    @service_agent_counts = current_user.agents.reorder(nil).where(service_id: @services.map(&:id)).group(:service_id).count
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @services }
+    end
+  end
+
+  def destroy
+    @services = current_user.services.find(params[:id])
+    @services.destroy
+
+    respond_to do |format|
+      format.html { redirect_to services_path }
+      format.json { head :no_content }
+    end
+  end
+
+  def toggle_availability
+    @service = current_user.services.find(params[:id])
+    @service.toggle_availability!
+
+    respond_to do |format|
+      format.html { redirect_to services_path }
+      format.json { render json: @service }
+    end
+  end
+
+end
